0fb2053f59e9c437ab6653944e956611580acf5f
Remi <remiclaeys.utc@gmail.com>
2015-09-08 21:43:25 +0200

    Ajout mascaret.kv

--- Full New/user/outil.py
+++ Full New/user/outil.py
@@ -1,5 +1,9 @@
 from gui.hoverclasses import HoverButton
-from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import ObjectProperty
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.adapters.listadapter import ListAdapter
+from kivy.uix.listview import ListItemButton, ListView, ListItemLabel
+from kivy.uix.boxlayout import BoxLayout
 
 
 class HPOutilsButton(HoverButton):
@@ -23,7 +27,7 @@ class HPOutilsButton(HoverButton):
             print("rrrr")
             self.parent.parent.parent.parent.manager.transition.direction = 'left'
             self.parent.parent.parent.parent.manager.current = "2"
-        
+
 
     def define_linked_tool(self):
 
@@ -47,6 +51,12 @@ class HPOutilsButton(HoverButton):
 
             return Liste_Commandes()
 
+        elif self.linkedoutils_name == "Formulaire dtb 1":
+
+            return Formulaire_database_1()
+
+
+
 
 
 class Outil:
@@ -56,17 +66,74 @@ class Outil:
 
 
 
-class Forecast_Tool(FloatLayout):
+class Forecast_Tool(RelativeLayout):
     pass
 
-class Liste_Employes(FloatLayout):
+class Liste_Employes(RelativeLayout):
     pass
 
-class CJSL01(FloatLayout):
+class CJSL01(RelativeLayout):
     pass
 
-class Creation_Facture(FloatLayout):
+class Creation_Facture(RelativeLayout):
     pass
 
-class Liste_Commandes(FloatLayout):
+class Liste_Commandes(RelativeLayout):
     pass
+
+class Formulaire_database_1(RelativeLayout):
+    new_legal_entity_box = ObjectProperty()
+
+    def send_new_legal_entity(self):
+        print(self.new_legal_entity_box.text)
+
+
+
+
+
+
+
+class TestListItem(ListItemButton):
+    pass
+
+class TestList(BoxLayout):
+##    list_view = ObjectProperty()
+##
+##    def force_list_view_update(self):
+##        self.list_view.adapter.update_for_new_data()
+##        self.list_view._trigger_reset_populate()
+##
+##    def roster_converter(self, row_index, rec):
+##        return {'text': rec['text'],'size_hint_y': None, 'height': 25}
+##
+##    ListAdapter(data=[{'text': str(i), 'is_selected': False} for i in range(100)],
+##                   cls=lb, args_converter=root.roster_converter)
+##
+##    self.adapter = ListAdapter(data=[{'text': str(i), 'is_selected': False} for i in range(100)],
+##                           args_converter=self.roster_converter,
+##                           propagate_selection_to_data=True,
+##                           cls=ListItemButton)
+
+    def __init__(self, *args, **kwargs):
+        super(TestList, self).__init__()
+        data = [{'text': str(i), 'is_selected': False} for i in range(100)]
+
+        args_converter = lambda row_index, rec: {'text': rec['text']}
+
+        list_adapter = ListAdapter(data=data,
+                                   args_converter=args_converter,
+                                   cls=TestListItem,
+                                   selection_mode='single',
+                                   allow_empty_selection=False)
+
+        list_view = ListView(adapter=list_adapter)
+        self.add_widget(list_view)
+
+
+
+
+
+
+
+
+
